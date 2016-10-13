0575bbb5511d1adb518b9f7acdd79f74afb2aeb8
Tomasz Rybarczyk <paluho@gmail.com>
2016-10-13 18:14:47 +0200

    Add dbus to preinstalled packages in debian rootfs recipe

--- src/Cook/Catalog/Debian/Rootfs.hs
+++ src/Cook/Catalog/Debian/Rootfs.hs
@@ -1,4 +1,8 @@
-module Cook.Catalog.Debian.Rootfs (buildRootfs, buildRootfsFromCache) where
+module Cook.Catalog.Debian.Rootfs (
+    buildRootfs
+  , buildRootfs'
+  , buildRootfsFromCache
+  ) where
 
 import Cook.Catalog.Debian.Debootstrap (debootstrap, defaults, include, Options, variant, Variant(Minbase))
 import Cook.Catalog.Debian.Suite (showSuite, stable, Suite)
@@ -11,34 +15,40 @@ import Data.List (isSuffixOf)
 import Data.Monoid ((<>))
 
 type RootFs = FilePath
+type Package = String
 
-packages = [ "init"
-           , "ifupdown"
-           , "locales"
-           , "dialog"
-           , "isc-dhcp-client"
-           , "netbase"
-           , "net-tools"
-           , "iproute"
-           , "openssh-server"
-           ]
+defaultPackages :: [Package]
+defaultPackages = [ "init"
+                  , "ifupdown"
+                  , "locales"
+                  , "dialog"
+                  , "isc-dhcp-client"
+                  , "netbase"
+                  , "net-tools"
+                  , "iproute"
+                  , "openssh-server"
+                  , "dbus"
+                  ]
 
-runDebootstrap :: Suite -> FilePath -> Recipe ()
-runDebootstrap suite path =
-  run (debootstrap defaults{include=packages} suite path Nothing)
+runDebootstrap :: Suite -> RootFs -> [Package] -> Recipe ()
+runDebootstrap suite path extraPkgs =
+  run (debootstrap defaults{include=extraPkgs} suite path Nothing)
 
-buildRootfs :: Suite -> FilePath -> Recipe FilePath
-buildRootfs suite path = withRecipeName "Debian.Rootfs.BuildRootfs" $ do
-  runDebootstrap suite path
+buildRootfs :: Suite -> RootFs -> [Package] -> Recipe FilePath
+buildRootfs suite path extraPkgs = withRecipeName "Debian.Rootfs.BuildRootfs" $ do
+  runDebootstrap suite path extraPkgs
   return path
+
+buildRootfs' :: Suite -> RootFs -> Recipe FilePath
+buildRootfs' suite path = buildRootfs suite path defaultPackages
 
 type CacheDir = FilePath
 
-buildRootfsFromCache :: Suite -> CacheDir -> RootFs -> Recipe RootFs
-buildRootfsFromCache suite cache path = withRecipeName "Debian.Rootfs.BuildRootfsFromCache" $ do
+buildRootfsFromCache :: CacheDir -> Suite -> RootFs -> Recipe RootFs
+buildRootfsFromCache cache suite path = withRecipeName "Debian.Rootfs.BuildRootfsFromCache" $ do
   let suiteCache = cache </> (addTrailingPathSeparator . showSuite $ suite)
   liftIO $ createDirectoryIfMissing True cache
   suiteCached <- liftIO $ doesDirectoryExist suiteCache
-  unless suiteCached (void (runDebootstrap suite suiteCache))
+  unless suiteCached (void (runDebootstrap suite suiteCache defaultPackages))
   runProc "rsync" ["-H", "-a", suiteCache, path]
   return path
